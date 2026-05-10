7b7cc1b2988aadfb0e137979978fbdc774bfd1ef
jsgiaverini <juliosoto2011@gmail.com>
2026-05-10 01:33:59 -0400

    Agrega microservicio base ms-auditoria

---
+++ ms-auditoria/src/main/java/cl/sanosysalvos/auditoria/MsAuditoriaApplication.java
@@ -0,0 +1,13 @@
+package cl.sanosysalvos.auditoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuditoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAuditoriaApplication.class, args);
+	}
+
+}
